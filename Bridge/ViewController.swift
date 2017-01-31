2fb70f76ea0072d02d4886f05f06e43881a64130
Blake Tsuzaki <dtsuzaki@scu.edu>
2017-01-30 16:01:26 -0800

    Initial Commit

---
+++ Bridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bridge
+//
+//  Created by Blake Tsuzaki on 1/30/17.
+//  Copyright © 2017 PRAHVI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
